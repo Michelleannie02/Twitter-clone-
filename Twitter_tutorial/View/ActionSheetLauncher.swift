210537e697aa1ff1f6dcdd1d1c3beb1bdbc00c93
rita <kajirita2002@gmail.com>
2020-04-04 16:56:08 +0900

    [add]ActionSheetLauncher

---
+++ Twitter_tutorial/View/ActionSheetLauncher.swift
@@ -0,0 +1,24 @@
+//
+//  ActionSheetLauncher.swift
+//  Twitter_tutorial
+//
+//  Created by 梶村律太 on 2020/04/04.
+//  Copyright © 2020 梶村律太. All rights reserved.
+//
+
+import UIKit
+
+class ActionSheetLancher: NSObject {
+    // MARK: - Properties
+    
+    private let user: User
+    init(user: User) {
+        self.user = user
+        super.init()
+    }
+    // MARK: - Helpers
+    
+    func show(){
+        
+    }
+}
