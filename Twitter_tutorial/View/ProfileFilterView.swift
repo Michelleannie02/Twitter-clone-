4a44f092953913f7030480cfb2fb7d1b84a0defe
rita <kajirita2002@gmail.com>
2020-04-05 22:57:11 +0900

    [update]FilterView Refactor

--- Twitter_tutorial/View/ProfileFilterView.swift
+++ Twitter_tutorial/View/ProfileFilterView.swift
@@ -26,6 +26,11 @@ class ProfileFilterView:UIView{
         cv.dataSource = self
         return cv
     }()
+    private let underlineView :UIView = {
+        let view = UIView()
+        view.backgroundColor = .twitterBlue
+        return view
+    }()
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
@@ -39,7 +44,11 @@ class ProfileFilterView:UIView{
         addSubview(collectionView)
         collectionView.addConstraintsToFillView(self)
     }
-    
+    override func layoutSubviews() {
+        addSubview(underlineView)
+        underlineView.anchor(left: leftAnchor, bottom: bottomAnchor,
+                             width: frame.width / 3,height: 2)
+    }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -63,7 +72,17 @@ extension ProfileFilterView:UICollectionViewDataSource{
 
 extension ProfileFilterView:UICollectionViewDelegate{
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        
+        let xPosition = cell?.frame.origin.x ?? 0
+        
+        UIView.animate(withDuration: 0.3) {
+            self.underlineView.frame.origin.x = xPosition
+        }
+        
+        
         delegate?.filterView(self, didSelect: indexPath)
+        
     }
 }
 // MARK: - UICollectionViewDelegateFlowLayout
