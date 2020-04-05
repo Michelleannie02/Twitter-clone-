91156973e10656dcea79eb01382f827ed867adcc
rita <kajirita2002@gmail.com>
2020-04-05 23:41:20 +0900

    [update]fetching liked Tweet

--- Twitter_tutorial/Controller/ProfileController.swift
+++ Twitter_tutorial/Controller/ProfileController.swift
@@ -50,6 +50,7 @@ class ProfileController: UICollectionViewController{
         super.viewDidLoad()
         configureCollectionView()
         fetchTweets()
+        fetchLikedTweets()
         checkIfUserIsFollowed()
         fetchUserStats()
         
@@ -64,6 +65,11 @@ class ProfileController: UICollectionViewController{
         TweetService.shared.fetchTweets(for: user) { tweets in
             self.tweets = tweets
             self.collectionView.reloadData()
+        }
+    }
+    func fetchLikedTweets(){
+        TweetService.shared.fetchLikes(forUser: user) { tweets in
+            self.likedTweets = tweets
         }
     }
     func checkIfUserIsFollowed() {
@@ -86,6 +92,9 @@ class ProfileController: UICollectionViewController{
         collectionView.contentInsetAdjustmentBehavior = .never
         collectionView.register(TweetCell.self, forCellWithReuseIdentifier: reuseIdentifier)
         collectionView.register(ProfileHeader.self,forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerIdentifier)
+        
+        guard let tabHeight = tabBarController?.tabBar.frame.height else { return }
+        collectionView.contentInset.bottom = tabHeight
     }
 }
 
@@ -117,7 +126,9 @@ extension ProfileController:UICollectionViewDelegateFlowLayout{
         return CGSize(width: view.frame.width, height: 350)
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 120)
+        let viewModel = TweetViewModel(tweet: tweets[indexPath.row])
+        let height = viewModel.size(forWidth: view.frame.width).height
+        return CGSize(width: view.frame.width, height: height + 72)
     }
     
 }
