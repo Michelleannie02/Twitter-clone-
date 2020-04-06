d9475096a9dc8d0163674acf245947e2d24d7fba
rita <kajirita2002@gmail.com>
2020-04-07 03:30:51 +0900

    [update] Set Edit Profile

---
+++ Twitter_tutorial/Controller/EditProfileController.swift
@@ -0,0 +1,68 @@
+//
+//  EditProfileController.swift
+//  Twitter_tutorial
+//
+//  Created by 梶村律太 on 2020/04/07.
+//  Copyright © 2020 梶村律太. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileController: UITableViewController {
+    // MARK: - Properties
+    private let user: User
+    private lazy var headerView = EditProfileHeader(user: user)
+    
+    // MARK: - Lifecycle
+    
+    init(user: User) {
+        self.user = user
+        super.init(style: .plain)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureNavigationBar()
+        configureTableView()
+    }
+    // MARK: - Selectors
+    
+    @objc func handleCancel(){
+        dismiss(animated: true, completion: nil)
+    }
+    @objc func handleDone(){
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - API
+    
+    // MARK: - Helpers
+    func configureNavigationBar() {
+        navigationController?.navigationBar.barTintColor = .twitterBlue
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.tintColor = .white
+        
+        navigationItem.title = "Edit Profile"
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(handleCancel))
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(handleDone))
+        navigationItem.rightBarButtonItem?.isEnabled = false
+    }
+    func configureTableView() {
+        tableView.tableHeaderView = headerView
+        headerView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 180)
+        tableView.tableFooterView = UIView()
+        headerView.delegate = self
+    }
+}
+extension EditProfileController: EditProfileHeaderDelegate {
+    func didTapChangeProfilePhoto() {
+        
+    }
+}
--- Twitter_tutorial/Controller/ProfileController.swift
+++ Twitter_tutorial/Controller/ProfileController.swift
@@ -157,7 +157,10 @@ extension ProfileController:ProfileHeaderDelegate{
     
     func handleEditProfileFollow(_ header: ProfileHeader) {
         if user.isCurrentUser{
-            print("show edit profile controller")
+            let controller = EditProfileController(user: user)
+            let nav = UINavigationController(rootViewController: controller)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: true, completion: nil)
             return
         }
         if user.isFollowed {
